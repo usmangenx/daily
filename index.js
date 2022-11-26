212f488fea49e18553707274026ddb407cb64a2f
usmanjaved0110 <musmanjaved5552gmail.com>
2022-11-26 12:01:32 +0500

    <checkSpeed>

--- index.js
+++ index.js
@@ -15,12 +15,25 @@
 
 // FizzBuzz Algorithm
 
+// function FizzBuzz(input) {
+//     if (typeof input !== 'number') return "not a number"
+//     if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz"
+//     if (input % 3 === 0) return "Fizz"
+//     if (input % 5 === 0) return "Buzz"
+//   return input
+// }
+// console.log(FizzBuzz(15))
 
-function FizzBuzz(input) {
-    if (typeof input !== 'number') return "not a number"
-    if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz"
-    if (input % 3 === 0) return "Fizz"
-    if (input % 5 === 0) return "Buzz"
-  return input
-}
-console.log(FizzBuzz(15))
+//checkSpeed
+
+checkSpeed = (speed) => {
+  const speedLimit = 70;
+  const speedPoints = 5;
+  if (speed <= speedLimit) return "ok";
+  else {
+    const points = Math.floor((speed - speedLimit) / speedPoints);
+    if (points >= 12) return "License Suspended";
+    else return "points" + " " + points;
+  }
+};
+console.log(checkSpeed(380));
