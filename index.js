76259471eca955214cf2c1787b244454016f1b7d
usmanjaved0110 <musmanjaved5552gmail.com>
2022-11-26 11:35:03 +0500

    <FizzBuzz Alorithm>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // get maximum number
 // maximumNumber = (num1 , num2)   =>   {
-//       if (num1>num2) 
+//       if (num1>num2)
 //         return num1;
 //         return num2;
 //        }
@@ -8,7 +8,19 @@
 
 // Check if landscape
 
-function isLandscape (width, height)  {
-    return (width > height) ;
+// function isLandscape (width, height)  {
+//     return (width > height) ;
+// }
+// console.log(isLandscape(240, 250));
+
+// FizzBuzz Algorithm
+
+
+function FizzBuzz(input) {
+    if (typeof input !== 'number') return "not a number"
+    if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz"
+    if (input % 3 === 0) return "Fizz"
+    if (input % 5 === 0) return "Buzz"
+  return input
 }
-console.log(isLandscape(240, 250));
+console.log(FizzBuzz(15))
