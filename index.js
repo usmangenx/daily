915767e02b53f228648732ea99cff3b2875869a4
usmanjaved0110 <musmanjaved5552gmail.com>
2022-11-26 11:10:16 +0500

    <commit_message>

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // get maximum number
-maximumNumber = (num1 , num2)   =>   {
-      if (num1>num2) 
-        return num1;
-        return num2;
-       }
-      console.log(maximumNumber(6, 4));
+// maximumNumber = (num1 , num2)   =>   {
+//       if (num1>num2) 
+//         return num1;
+//         return num2;
+//        }
+//       console.log(maximumNumber(6, 4));
+
+// Check if landscape
+
+function isLandscape (width, height)  {
+    return (width > height) ;
+}
+console.log(isLandscape(240, 250));
