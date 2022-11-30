f8d28da979622e4843dd4ae397cef43a3a3cc5ec
Muhammad Usman Javed <musmanjaved5552@gmail.com>
2022-12-01 01:12:00 +0500

    <fizzbuzz>

---
+++ fizzbuzz.js
@@ -0,0 +1,10 @@
+// FizzBuzz Algorithm
+
+function FizzBuzz(input) {
+    if (typeof input !== 'number') return "not a number"
+    if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz"
+    if (input % 3 === 0) return "Fizz"
+    if (input % 5 === 0) return "Buzz"
+  return input
+}
+console.log(FizzBuzz(15))
--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// FizzBuzz Algorithm
-
-// function FizzBuzz(input) {
-//     if (typeof input !== 'number') return "not a number"
-//     if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz"
-//     if (input % 3 === 0) return "Fizz"
-//     if (input % 5 === 0) return "Buzz"
-//   return input
-// }
-// console.log(FizzBuzz(15))
-
 //checkSpeed
 
 // checkSpeed = (speed) => {
