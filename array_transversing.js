75933ed36d0e8cbbceaf2e0c84611c413b579d68
Muhammad Usman Javed <musmanjaved5552@gmail.com>
2022-12-07 03:06:26 +0500

    <mer>

--- array_transversing.js
+++ array_transversing.js
@@ -1,11 +1,5 @@
 let arry = [23, 30, 40, 54];
 let arry2 = [50, 18, 16, 23];
-let arry3 = [];
-for (i = 0; i < arry.length; i++) {
-  arry3[i] = arry[i];
-}
-for (i = 0; i < arry2.length; i++) {
-  arry3[arry.length + i] = arry2[i];
-}
+let arry3 = [...arry,...arry2];
 
-console.log(arry3);
+console.log(arry3)
